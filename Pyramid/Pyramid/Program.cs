5e9608d374ee9e8686bddde912f79d9adf40eb1d
Subasish Mohapatra <subasish.mohapatra@hotmail.com>
2020-01-19 14:21:02 +0530

    Updated input and output

--- Pyramid/Pyramid/Program.cs
+++ Pyramid/Pyramid/Program.cs
@@ -5,51 +5,54 @@ namespace Pyramid
 {
     class Program
     {
-        //Input
-        //1st line: no of test cases
-        //2nd line: "input string  to print" "height of pyramid" "inversePyramid" e.g., abc 4 1 or abcdefg 4 -1
+        /* 
+        Input
+        1st line: no of test cases
+        2nd line: "input string  to print" "height of pyramid" "inversePyramid" e.g., abc 4 1 or abcdefg 4 -1
+        Output: Print string in reverse order every line
+
+            abc 4 1
+
+               a
+              acb
+             bcabc
+            acbacba
+
+           =============
+
+           abc 4 -1
+
+           abcabca
+            cbacb
+             abc
+              a
+
+        */
         static void Main(string[] args)
         {
-            /* 
-             abc 4 1
-            
-                a
-               acb
-              bcabc
-             acbacba
-
-            =============
-
-            abc 4 -1
-
-            abcabca
-             cbacb
-              abc
-               a
-
-             */
+           
             Console.WriteLine("Enter no of test cases");
-            var t = int.Parse(Console.ReadLine());
-            while (t-- > 0)
+            var noOfTestCases = int.Parse(Console.ReadLine());
+            while (noOfTestCases-- > 0)
             {
                 Console.WriteLine("\"Enter string to display\" \"height of pyramid\" \"inverse pyramid or not\"");
                 var nextInput = Console.ReadLine().Split(" ");
                 var inputString = nextInput[0];// "abc";
                 var heightOfPyramid = int.Parse(nextInput[1]); //4;
-                var pyramidDirection = int.Parse(nextInput[2]); //1;
-                var inputStringLength = inputString.Length;
-                var totalCharacters = heightOfPyramid * heightOfPyramid;
-                var repeatString = totalCharacters / inputStringLength;
-                var noOfExtraCharacters = totalCharacters % inputStringLength;
-                var finalString = string.Concat(Enumerable.Repeat(inputString, repeatString)) + inputString.Substring(0, noOfExtraCharacters);
-                var lastLineLength = (heightOfPyramid * 2) - 1;
-                PrintDisplay(heightOfPyramid, finalString, lastLineLength, pyramidDirection!=1);
+                var pyramidDirection = int.Parse(nextInput[2]); //1;               
+                PrintDisplay(heightOfPyramid, inputString, pyramidDirection!=1);
             }
             Console.ReadKey();
         }
 
-        private static void PrintDisplay(int h, string finalString, int lastLineLength,  bool reversePyramid)
+        private static void PrintDisplay(int heightOfPyramid, string inputString, bool reversePyramid)
         {
+            var inputStringLength = inputString.Length;
+            var totalCharacters = heightOfPyramid * heightOfPyramid;
+            var repeatString = totalCharacters / inputStringLength;
+            var noOfExtraCharacters = totalCharacters % inputStringLength;
+            var finalString = string.Concat(Enumerable.Repeat(inputString, repeatString)) + inputString.Substring(0, noOfExtraCharacters);
+            var lastLineLength = (heightOfPyramid * 2) - 1;
             var lastIndex = 0;
             var isReverse = true;
             int lineNo = 1;
@@ -57,10 +60,10 @@ namespace Pyramid
             int ctr = 1;
             if (reversePyramid)
             {
-                lineNo = h;
+                lineNo = heightOfPyramid;
                 increment = -1;
             }
-            while (ctr <= h)
+            while (ctr <= heightOfPyramid)
             {
                 var noOfCharsToPrint = lineNo + (lineNo - 1);
                 var printString = finalString.Substring(lastIndex, noOfCharsToPrint);
